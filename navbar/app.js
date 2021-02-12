164f5e2d5af9f5e9de165eb811f5dcd96e12e8bd
Antony Casado <antony-casado@outlook.com>
2021-02-12 13:26:32 +0100

    short version with .toggle

--- navbar/app.js
+++ navbar/app.js
@@ -8,24 +8,5 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener('click', function(){
-    myFunction();
-    
+links.classList.toggle("show-links");
 })
-
-
-
-
-function myFunction() {
-
-    if (links.classList.contains("show-links")){
-
-        var element1 = document.getElementById("myUl");
-        element1.classList.remove("show-links");
-
-    }
-
-
-    else {
-    var element = document.getElementById("myUl");
-    element.classList.add("show-links");
-}}
